b4f2601b50a9fe22a879aecc307cecb6e59a7909
Emmanuel Martínez <emmanuel.martinez@ucsp.edu.pe>
2025-06-17 10:51:30 -0500

    xd

--- Assets/Scripts/InstantiatePlayer.cs
+++ Assets/Scripts/InstantiatePlayer.cs
@@ -7,25 +7,31 @@ public class InstantiatePlayer : MonoBehaviour
 
     private static int playerCount = 0;
 
+    //void Start()
+    //{
+    //    GameObject prefabToSpawn;
+
+    //    if (playerCount == 0)
+    //    {
+    //        prefabToSpawn = heartPrefab;
+    //    }
+    //    else if (playerCount == 1)
+    //    {
+    //        prefabToSpawn = handsPrefab;
+    //    }
+    //    else
+    //    {
+    //        Debug.LogWarning("Más de 2 jugadores no están soportados.");
+    //        return;
+    //    }
+
+    //    Instantiate(prefabToSpawn, transform.position, transform.rotation);
+    //    playerCount++;
+    //}
+
     void Start()
     {
-        GameObject prefabToSpawn;
-
-        if (playerCount == 0)
-        {
-            prefabToSpawn = heartPrefab;
-        }
-        else if (playerCount == 1)
-        {
-            prefabToSpawn = handsPrefab;
-        }
-        else
-        {
-            Debug.LogWarning("Más de 2 jugadores no están soportados.");
-            return;
-        }
-
-        Instantiate(prefabToSpawn, transform.position, transform.rotation);
-        playerCount++;
+        Instantiate(heartPrefab, transform.position, transform.rotation);
+        Instantiate(handsPrefab, transform.position, transform.rotation);
     }
 }
